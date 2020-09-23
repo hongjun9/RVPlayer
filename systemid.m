a64c7f3b10516da7691a02d2e4f60ad9b3db887e
Hongjun Choi <hongjun922@gmail.com>
2020-09-23 01:06:55 -0400

    updated

--- systemid.m
+++ systemid.m
@@ -72,7 +72,7 @@ raw_states(:,12) = -raw_states(:,12);
 
 
 %%resample (for uniform sampling time)
-desiredFs = 400; %(default 400Hz)
+desiredFs = 10; %(default 400Hz)
 Ts = 1/desiredFs;
 [res_states, res_timestamps] = resample(raw_states,raw_timestamps,desiredFs);
 [res_motors, res_timestamps] = resample(raw_motors,raw_timestamps,desiredFs);
@@ -113,9 +113,9 @@ thetas = [deg2rad(45), deg2rad(-135), deg2rad(-45), deg2rad(135)];
 % c = 0.13;
 % d = 0.22;
 m = 1.5;
-I_x = 0.008; %16365151e-9; %16365151e-9;      % Inertia (kg*m^2)
+I_x = 0.015; %16365151e-9; %16365151e-9;      % Inertia (kg*m^2)
 I_y = 0.015; %8354114e-9;    %8354114e-9;
-I_z = 0.017; %24008439e-9;  %24008439e-9;
+I_z = 0.015; %24008439e-9;  %24008439e-9;
 
 
 % alpha = 9.6;
@@ -129,13 +129,13 @@ throttle_hover = 0.51; %0.5595;%0.51; %0.7;    % (%)  (1559-1100)/900=0.51
 % K_T = m*g / (4*(throttle_hover*900+1100));
 K_T = m*g / (4*throttle_hover);   % = 7.2108
 % K_Q = K_T*0.034;                % guess based on K_T 0.034;
-K_Q = yaw_scale * I_z;
+K_Q = yaw_scale * I_z;          % = 6.9813 * 0.015 = 0.1187
 
 %a=d, b=c
-a =  sin(thetas(1)) * arm_scale * I_x / K_T;    %0.7071 * 87.2665 * 0.008 / 7.2108 = 0.0685
-b =  cos(thetas(1)) * arm_scale * I_y / K_T;    %0.7071 * 87.2665 * 0.015 / 7.2108 = 0.1284
-c = -cos(thetas(4)) * arm_scale * I_y / K_T;    %0.7071 * 87.2665 * 0.015 / 7.2108 = 0.1284
-d =  sin(thetas(4)) * arm_scale * I_x / K_T;    %0.7071 * 87.2665 * 0.008 / 7.2108 = 0.0685
+a =  sin(thetas(1)) * arm_scale * I_x / K_T;    %0.7071 * 87.2665 * 0.015 / 7.2108 = 0.1284
+b =  cos(thetas(1)) * arm_scale * I_y / K_T;    
+c = -cos(thetas(4)) * arm_scale * I_y / K_T;    
+d =  sin(thetas(4)) * arm_scale * I_x / K_T;    
 
 %====================================
 % test the model implementation
@@ -151,11 +151,11 @@ for n=1:N-1
     x(6,n+1) = mod(x(6,n+1), 2*pi);
 %     x(10:12,n+1) = states(10:12,n+1);
 %     x(4:6,n+1) = states(4:6,n+1);
-    if n * dt < 10
-        x(10:12,n+1) = states(10:12,n+1);
-        x(4:6,n+1) = states(4:6,n+1);
-    end
-    
+%     if n * dt < 10
+%         x(10:12,n+1) = states(10:12,n+1);
+%         x(4:6,n+1) = states(4:6,n+1);
+%     end
+%     
     
 %     %k-step ahead estiamtion (sync at every-k loop)
 %     k = 10;
@@ -179,11 +179,11 @@ end
 % hold on; plot(t, x(14,:))
 % hold on; plot(t, x(15,:))
 % hold on; plot(t, x(16,:))
-return
+% return
 
 %====================================
-%% System Identification
-
+%% Nonlinear grey-box model - idnlgrey
+%
 % data = iddata(states, motors, 'SamplingInstants', timestamps); %non-uniform sampling
 data = iddata(states', motors', Ts);    % uniform sampling (Ts)
 Filename       = 'quadrotor_m';                % File describing the model structure.
@@ -192,34 +192,6 @@ Parameters    = [a; b; c; d; m; I_x; I_y; I_z; K_T; K_Q];   % Initial parameter 
 InitialStates = x(:,1);
 nlgr_m    = idnlgrey(Filename, Order, Parameters, InitialStates);   %Nonlinear grey-box model
 
-%%SI options
-%
-opt = nlgreyestOptions;
-opt.Display = 'on';
-%opt.GradientOptions.DiffMaxChange              % default: Inf
-% opt.GradientOptions.DiffMinChange = 1e-3;    % defailt: 0.01*sqrt(eps) = 1.4901e-10
-%opt.GradientOptions.DiffScheme = 'auto';       % default: auto, 'Central approximation', 'Forward approximation', 'Backward approximation'
-% opt.GradientOptions.GradientType = 'auto';      % default: auto, 'Basic', 'Refined'
-opt.SearchMethod = 'gn';                        % default: auto(lsqnonlin), gn, gna, lm, grad, fmincon
-% opt.SearchOption.Tolerance = 1e-10;     %FunctionTolerance (1e-5) - gn
-% opt.SearchOption.Algorithm = 'interior-point';     % interior-point, sqp, active-set, trust-region-reflective, 
-% opt.SearchOption.TolFun = 1e-10;      %FunctionTolerance (1e-5)
-% opt.SearchOption.TolX = 1e-10;        %StepTolerance (1e-6)
-opt.SearchOption.MaxIter = 100;
-% opt.SearchOption.Advanced.GnPinvConst = 10000000;
-%opt.Regularization.Nominal = 'model';          %default: 'zero'
-
-%--------------------------------------------
-% outputweight = eye(NX);
-% outputweight(1,1) = 1;
-% outputweight(2,2) = 1;
-% outputweight(3,3) = 1;
-% outputweight(4,4) = 1000;
-% outputweight(5,5) = 1;
-% outputweight(6,6) = 1;
-% opt.OutputWeight = outputweight;
-
-%-----------------------------------------------------------
 %% Parameter setting
 % 
 nlgr_m.Parameters(1).Fixed = true;      %abcd
@@ -227,9 +199,9 @@ nlgr_m.Parameters(2).Fixed = true;
 nlgr_m.Parameters(3).Fixed = true;
 nlgr_m.Parameters(4).Fixed = true;
 nlgr_m.Parameters(5).Fixed = true;      %weight
-nlgr_m.Parameters(6).Fixed = true;      %Ix
-nlgr_m.Parameters(7).Fixed = true;      %Iy
-nlgr_m.Parameters(8).Fixed = true;      %Iz
+% nlgr_m.Parameters(6).Fixed = true;      %Ix
+% nlgr_m.Parameters(7).Fixed = true;      %Iy
+% nlgr_m.Parameters(8).Fixed = true;      %Iz
 % nlgr_m.Parameters(9).Fixed = true;     %thrust const  **
 % nlgr_m.Parameters(10).Fixed = true;    %torque_const  **
 %nlgr_m.Parameters(11).Fixed = true;    %K_m  **
@@ -266,36 +238,62 @@ nlgr_m = setpar(nlgr_m, 'Unit', {'m' 'm' 'm' 'm' 'kg' 'kg*m^2' 'kg*m^2' 'kg*m^2'
 % nlgr_m.SimulationOptions.AbsTol = 1e-10;
 % nlgr_m.SimulationOptions.RelTol = 1e-10;
 
-model = nlgreyest(data, nlgr_m, opt);    %Estimate nonlinear grey-box model parameters
+%% System Identification
+%%SI options ===================================================
+%
+opt = nlgreyestOptions;
+opt.Display = 'on';
+%opt.GradientOptions.DiffMaxChange              % default: Inf
+% opt.GradientOptions.DiffMinChange = 1e-3;    % defailt: 0.01*sqrt(eps) = 1.4901e-10
+%opt.GradientOptions.DiffScheme = 'auto';       % default: auto, 'Central approximation', 'Forward approximation', 'Backward approximation'
+% opt.GradientOptions.GradientType = 'auto';      % default: auto, 'Basic', 'Refined'
+opt.SearchMethod = 'gn';                        % default: auto(lsqnonlin), gn, gna, lm, grad, fmincon
+% opt.SearchOption.Tolerance = 1e-10;     %FunctionTolerance (1e-5) - gn
+% opt.SearchOption.Algorithm = 'interior-point';     % interior-point, sqp, active-set, trust-region-reflective, 
+% opt.SearchOption.TolFun = 1e-10;      %FunctionTolerance (1e-5)
+% opt.SearchOption.TolX = 1e-10;        %StepTolerance (1e-6)
+opt.SearchOption.MaxIter = 100;
+% opt.SearchOption.Advanced.GnPinvConst = 10000000;
+%opt.Regularization.Nominal = 'model';          %default: 'zero'
 
-return
+%--------------------------------------------
+% outputweight = eye(NX);
+% outputweight(1,1) = 1;
+% outputweight(2,2) = 1;
+% outputweight(3,3) = 1;
+% outputweight(4,4) = 1000;
+% outputweight(5,5) = 1;
+% outputweight(6,6) = 1;
+% opt.OutputWeight = outputweight;
+
+% model = nlgreyest(data, nlgr_m, opt);    %Estimate nonlinear grey-box model parameters
+model = nlgr_m;
+% present(model);
+% return
 
 %%===================================================
 %% plot results
 %%
 
-present(model);
-yy = sim(model,data);
-% figure; yy = compare(model, data);
-
-output = yy.OutputData';
-% N = size(output,2);
-% figure;
-% for n=1:NY
-%     if NOUT > 1
-%         ax = subplot(NY/NCOL, NCOL, n);
-%     end
-%     plot(t, states(n,:), 'r--', t, output(n,sp:ep), 'b-');   
-% %     ylim([-1.5 1.5]);
-% %     title(title_name(n));
-% end
+% sim vs prediction
+input_data = iddata([], motors', Ts);
+ys = sim(model,input_data);
+K = 10; % K-step-ahead prediction
+yp = predict(nlgr_m, data, K);
 
 figure;
 for n=1:NY
-    subplot(NY/3, 3, n);
-    plot(timestamps,states(n,:),'b.-');
+    if NY > 1
+        subplot(NY/3, 3, n);
+    end
+    plot(timestamps, data.y(:,n), 'k-');
+    hold on; 
+    plot(timestamps, ys.y(:,n),'r.-');
     hold on;
-    plot(timestamps, output(n,:),'r--');
-    legend('Original','Model');
+    plot(timestamps, yp.y(:,n), 'b.');  
+    legend('truth', 'simulated', 'predicted');
     title(title_name(n));
 end
+
+figure; 
+compare(model, data);
