78355539627341fab5eb88df00aef33ef315698e
Bob <bobchengzy@gmail.com>
2021-03-12 16:31:32 -0500

    add rover data

--- ClipData.m
+++ ClipData.m
@@ -1,10 +1,17 @@
-function [test_data] = ClipData(test_data, reference_motor, start_offset, end_offset, max_freq)
+function [test_data] = ClipData(vehicle_type, test_data, reference_motor, start_offset, end_offset, max_freq)
 % Clip the data and keep only the part with motor signal higher than
+% vehicle type: %0: simquad, 1: solo, 2: simrover, 3: erle rover
 % reference_motor. Initial time is the time first reaching reference_motor.
 % start_offset: clip offset of starting time. Unit: s
 % end_offset: clip offset of ending time. Unit: s
 
-refer_idx = find(test_data(:, 14) >= reference_motor, 1);
+if vehicle_type == 0 || vehicle_type == 1
+    refer_idx = find(test_data(:, 14) >= reference_motor, 1);
+elseif vehicle_type == 2 || vehicle_type == 3
+    refer_idx = find(abs(test_data(:, 16)-0.5) >= reference_motor, 1);
+else
+    error("unknown vehicle type");
+end
 reference_time = test_data(refer_idx, 1); % test_data(1, 1)
 test_data(:, 1) = test_data(:, 1)-reference_time; % reset start time
 %trim data (remove unnecessary parts)
